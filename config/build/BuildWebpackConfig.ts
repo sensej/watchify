ef83d2b2a1ff2cb80748f510b5cca4f70f9f55cd
Ramazan Vlilyaev <itsensej@gmail.com>
2023-08-18 11:37:29 +0300

    added folder config build for webpack

---
+++ config/build/BuildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function BuildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { mode, paths, isDev } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+  };
+}
